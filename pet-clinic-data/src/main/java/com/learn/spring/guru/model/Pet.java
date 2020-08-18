d556ea4e98f1aa587acbe2ed5993f4245ae017b8
SKRaja88 <sathishtprl@gmail.com>
2020-08-18 08:58:07 +0200

    updated vet and speciality as JPA entities

--- pet-clinic-data/src/main/java/com/learn/spring/guru/model/Pet.java
+++ pet-clinic-data/src/main/java/com/learn/spring/guru/model/Pet.java
@@ -2,6 +2,8 @@ package com.learn.spring.guru.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="pets")
@@ -21,8 +23,27 @@ public class Pet extends BaseEntity {
     @Column(name="birth_date")
     private LocalDate birthDate;
 
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits=new HashSet();
+
     public PetType getPetType() {
         return petType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 
     public void setPetType(PetType petType) {
--- pet-clinic-data/src/main/java/com/learn/spring/guru/model/Visit.java
+++ pet-clinic-data/src/main/java/com/learn/spring/guru/model/Visit.java
@@ -1,13 +1,20 @@
 package com.learn.spring.guru.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name="visits")
 public class Visit extends BaseEntity {
 
+    @Column(name="date")
     private LocalDate date;
+
+    @Column(name="description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name="pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
