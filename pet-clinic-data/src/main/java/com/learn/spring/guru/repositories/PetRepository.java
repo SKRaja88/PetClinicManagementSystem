ce64c36f927219bd91fb09a4553899f1dfd32172
SKRaja88 <sathishtprl@gmail.com>
2020-08-18 19:32:52 +0200

    Repos

--- pet-clinic-data/src/main/java/com/learn/spring/guru/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/com/learn/spring/guru/repositories/PetRepository.java
@@ -1,6 +1,7 @@
 package com.learn.spring.guru.repositories;
 
 import com.learn.spring.guru.model.Owner;
+import com.learn.spring.guru.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PetRepository extends CrudRepository<Pet,Long> {
