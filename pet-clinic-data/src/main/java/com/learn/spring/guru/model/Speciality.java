2b31b8a20cf4f6342ed8f67a6a2d5aefd89f10d6
SKRaja88 <sathishtprl@gmail.com>
2020-08-18 08:36:51 +0200

    updated vet and speciality as JPA entities

--- pet-clinic-data/src/main/java/com/learn/spring/guru/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/learn/spring/guru/model/Speciality.java
@@ -1,10 +1,14 @@
 package com.learn.spring.guru.model;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="specialities")
 public class Speciality {
 
+    @JoinColumn(name="description")
     private String description;
 
     public String getDescription() {
